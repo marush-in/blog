7c110807074c63e3806fed8c53145a06e73e13b2
marush-in <shin.marumoto@gmail.com>
2019-09-15 09:31:52 +0900

    S3の設定を追加

--- app/config/settings.py
+++ app/config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'blog.apps.BlogConfig',
     'common.apps.CommonConfig',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -160,3 +161,12 @@ LOGGING = {
         }
     }
 }
+
+
+# AWS S3
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_REGION_NAME = os.environ.get('AWS_DEFAULT_REGION', 'ap-northeast-1')
+AWS_AUTO_CREATE_BUCKET = True
